f5d5f4e385b4e85bee834faccce1d6743b6bed60
Jonas Itaqui <jgsitaqui@gmail.com>
2020-12-10 05:10:11 -0300

    teste de Link1

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,13 @@
-import { useState } from 'react';
+import Link from 'next/link';
 
 function Home() {
-    return (
-        <div>
-            <h1> Home ddd </h1>
-            <Contador />
-        </div>
-    )
+    return (<div>
+        <h1>Home</h1>
+
+        <Link href="/sobre">
+            <a>Acessar página Sobre</a>
+        </Link>
+    </div>)
 }
-function Contador() {
-    const [contador, setContador] = useState(1);
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adiciona</button>
-        </div>
-    )
-}
+
 export default Home
